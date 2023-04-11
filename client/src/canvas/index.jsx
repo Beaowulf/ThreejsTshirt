7e80d078b1ff809f6903f8414e0df0e977d23e37
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2023-04-11 20:28:30 +0300

    G

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+
+const CanvasModel = () => {
+  return <div>CanvasModel</div>;
+};
+
+export default CanvasModel;
