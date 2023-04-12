d897d054e8d38100c1790dad4f20756729114312
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2023-04-12 16:14:47 +0300

    fixedheader

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json({ message: "Hello from DALL.E" });
 });
 
